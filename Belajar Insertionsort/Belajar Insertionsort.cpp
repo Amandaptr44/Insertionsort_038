e6fafe0720e85ceef35871219e23bc78495abec9
Amandaptr44 <amandaptr86@gmail.com>
2023-03-23 10:59:05 +0700

    Prosedur input

--- Belajar Insertionsort/Belajar Insertionsort.cpp
+++ Belajar Insertionsort/Belajar Insertionsort.cpp
@@ -2,4 +2,31 @@
 using namespace std;
 
 int arr[20];			// Membuat Array dengan panjang data 20
-int n;					// Membuat variable inputan n 
+int n;				    // Membuat variable inputan n 
+
+void input() {          // Prpcedure Input
+    while (true) {
+
+        cout << "Masukan banyaknya elemen pada array : ";  // Membuat Inputan jumlah element Array 
+        cin >> n;                                          // memanggil variabel inputan n
+
+        if (n <= 20) {                                     // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl;                            // Membuat jarak per baris program 
+    cout << "===================" << endl;   // Membuat tampilan susunan data element array
+    cout << "Masukan Elemen Array" << endl;  
+    cout << "===================" << endl;   
+
+    for (int i = 0; i < n; i++)                     // Menggunakan perulangan for untuk menyimpan data pada array
+        {
+        cout << "Data ke-" << (i + 1) << ": ";     // Memasukkan atau menginputkan nilai data a
+        cin >> arr[i];                             // Menyimpan nilai data n kedalaman array arr
+    }
+}
+
