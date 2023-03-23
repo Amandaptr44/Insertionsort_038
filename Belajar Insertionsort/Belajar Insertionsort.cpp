a19d7c5c66f8b0dfd6885542561fa3fe499ea3ee
Amandaptr44 <amandaptr86@gmail.com>
2023-03-23 11:44:50 +0700

    Prosedur display. Lalu memanggil prosedur input, insertionsort, dan display untuk ditampilkan

--- Belajar Insertionsort/Belajar Insertionsort.cpp
+++ Belajar Insertionsort/Belajar Insertionsort.cpp
@@ -65,7 +65,7 @@ void display() {                                   //Procedure Display
     cout << "==================================" << endl;   // Output ke layar
 
     for (int j = 0; j < n; j++) {            // Looping dengan j dimulai dari 0 hingga n-1
-        cout << a[j] << endl;                // Output ke layar
+        cout << arr[j] << endl;                // Output ke layar
     }
     cout << endl;                            // Output baris kosong
 }
